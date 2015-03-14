3edcbb03a53e126739543ff313ae58d24457290f
mesfin <mesfin_meng@yahoo.com>
2015-03-14 18:22:16 +0200

    Jquery to add lists are updated

--- js/script.js
+++ js/script.js
@@ -5,9 +5,21 @@ $(document).ready(function() {
     //Event Listner to SAVE button
 
     $(".save_btn ").click( function () {
-            $( '.list_items ').append(' <li>mess</li>');
-            $( '.item_unit ').append(' <li>2kg</li>');
-            $('.item_section').show();
+        $('.item_section').show();
+
+       var item = $(' #add_item  ').val();
+         $( '.list_items ').append( ' <li>' + item   +  ' </li> ' );
+         $(' #add_item').val(item);
+          var n_item = $(' #qty').val();
+          $( '.item_unit ').append ( '  <li>  ' + n_item + '  </li> ' );
+          $(' #qty').val(n_item);
+       //var item = $( this  ).val();
+        //$(' #qty').val(n_item);
+
+
+          // $( '.item_unit ').append ( '  <li>  ' + n_item + '  </li> ' );
+
+
 
          //alert('Save button is clicked' );
     });
