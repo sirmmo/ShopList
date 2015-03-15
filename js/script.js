df461c89a41882c2b22087642440576f72b693d0
mesfin <mesfin_meng@yahoo.com>
2015-03-15 12:48:54 +0200

    Now Users input can be listed out

--- js/script.js
+++ js/script.js
@@ -10,16 +10,28 @@ $(document).ready(function() {
     $('.save_btn').click( function () {
 
         $('.item_section').show();
+
        var item = $(' #add_item[type=text]  ').val();
-      $( '.list_items ').append( ' <li>' + item   +  ' </li> ' );
+        var n_item = $(' #qty[type=text]').val();
+         $( '.list_items ').append( ' <li>' + item   +  ' </li> ' );
+       $( '.item_unit ').append ( '  <li>  ' + n_item + '  </li> ' );
+        $( '.del  ').append( ' <li>' + '<a href = "#"> ' + 'delete'  + '</a>'  +  ' </li> ' );
+         $( '.edit  ').append( ' <li>' + '<a href = "#"> ' + 'edit'  + '</a>'  +  ' </li> ' );
+         $( '.check_out  ').append( ' <li>' + '<a href = "#"> ' + 'check'  + '</a>'  +  ' </li> ' );
 
-           var n_item = $(' #qty[type=text]').val();
-          $( '.item_unit ').append ( '  <li>  ' + n_item + '  </li> ' );
-           return false; //Prevents the page not to reload every time item sinserted
 
-         //alert('Save button is clicked' );
-    });
-//Even Listnet to RESET button
+/*$(' .del').on('click ' , function () {
+        $( '.del  ').append( ' <li>' + '<a href = "#"> ' + 'delete'  + '</a>'  +  ' </li> ' );
+                $(this).closest('li').remove();
+        //$('.list_items')[0].remove();
+         //$('.item_unit')[0].remove();
+    });*/
+
+         return false; //Prevents the page not to reload every time item sinserted
+
+ });
+
+//Empty current input form
      $(".reset_btn ").click( function () {
          //alert('RESET button is clicked' );
          $('.list_items')[0].reset();
