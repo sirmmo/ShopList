0ca5d723b4b59cab7733d62350cadcc23f6b90f2
mesfin <mesfin_meng@yahoo.com>
2015-03-18 22:43:53 +0200

    images are added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function() {
-
+ $(' #error').hide();
     //Add items
 
     //Event Listner to SAVE button
@@ -9,52 +9,39 @@ $(document).ready(function() {
     //
     $('.save_btn').click( function () {
 
-        $('.item_section').show();
+           if($('input' ).val().trim().length == 0){
+                   $(' #error').show();
 
-       var item = $(' #add_item[type=text]  ').val();
-        var n_item = $(' #qty[type=text]').val();
-        if (item <1) {
 
-        };
-         /*$( '.list_items ').append( ' <li>' + item   +  ' </li> ' );
-       $( '.item_unit ').append ( '  <li>  ' + n_item + '  </li> ' );
-        $( '.del  ').append( ' <li>' + '<a href = "#"> ' + 'delete'  + '</a>'  +  ' </li> ' );
-         $( '.edit  ').append( ' <li>' + '<a href = "#"> ' + 'edit'  + '</a>'  +  ' </li> ' );
-         $( '.check_out  ').append( ' <li>' + '<a href = "#"> ' + 'check'  + '</a>'  +  ' </li> ' );*/
+            }else{
+
+        var item = $(' #add_item ').val();
+        var n_item = $(' #qty').val();
 
          $( 'ol').append ('<li> <span class="list_items">' + item + '</span> ' +
             '<span class="item_unit">'+ n_item+ '</span>' +
-            '<span class="del">delete</span>' +
-           '<span class=" edit "> edit</span>' +
-           '<span class=" check "> check</span> </li>' );
+            '<span class="del" ><img class = "delimg"src="images/del.png" alt= "delete Item" /></span>' +
+           '<span class=" edit "> <img class = "editimg"src="images/edit2.png" alt = "Edit item" /></span>' +
+           '<span class=" check "> <img class = "checkimg"src="images/check.png"  alt = "check out Item"/></span> </li>' );
+         $('.item_section').show();
+          $(' #error').hide();
 
-            $(' .del').on('click ' , function () {
+}
+
+           $(' .del').on('click ' , function () {
                  $(this).closest('li').remove();
-
-    });
-            $(' .edit').on('click', function() {
-                 var item = $(' #add_item[type=text]  ').val();
-                    var n_item = $(' #qty[type=text]').val();
-
-                $('input').val(item);
-                $('input').val(n_item);
-
-                //var item = $(' #add_item[type=text]').val();
-               //var n_item = $(' #qty[type=text]').val();
-
-                //$('input').val(item);
-               // $('input').val(item);
-                 // $(this).closest('li').val();
-                  /* var item = $(' #add_item[type=text]  ').val();
-                    var n_item = $(' #qty[type=text]').val();
-                   $('.item_section').hide();
-                    $('form').closest('li').val();
-                    $('form').show();*/
-                     $('.item_section').hide();
-
+     });
+            $(' .edit').click (function() {
+               alert('Edit button detected');
+         $('.item_section').hide();
+});
+            $('.check').click(function () {
+                $(this).closest(' li').css('backgroundColor', '#D8D8D8');
 
 
             });
+
+
 
          return false; //Prevents the page not to reload every time item sinserted
 
